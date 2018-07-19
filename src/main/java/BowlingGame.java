54e3cd78bb1c53a5661e85ee3b52d3e389bcb737
ramesh.thippana <ramesh.thippana@aa.com>
2018-07-19 18:47:05 -0500

    Updated tests

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,17 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BowlingGame {
-    List<Integer> rolls = new ArrayList<>();
+    int[] rolls = new int[21];
+    int rollIndex = 0;
 
-    public void roll(int i) {
-        rolls.add(i);
+    public void roll(int pins) {
+        rolls[rollIndex++] = pins;
     }
 
     public int score() {
-        if(rolls.stream().limit(2).mapToInt(Integer::valueOf).sum()==10){
-            return 16;
+        int score = 0;
+        int index = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rolls[index])) {
+                score += 10 + rolls[index+1] + rolls[index + 2];
+                index += 1;
+            } else if (isSpare(index)) {
+                score += 10 + rolls[index + 2];
+                index += 2;
+            } else {
+                score += rolls[index] + rolls[index + 1];
+                index += 2;
+            }
         }
-        return rolls.stream().mapToInt(Integer::valueOf).sum();
+        return score;
     }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
+    }
+
+    private boolean isSpare(int index) {
+        return rolls[index] + rolls[index + 1] == 10;
+    }
+
+
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,34 +1,72 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
     BowlingGame game = new BowlingGame();
 
     @Test
     public void scoreShouldBeZeroIfGutterGame() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(20, 0);
         assertEquals(0, game.score());
     }
 
+
     @Test
     public void scoreShouldBe20If1IsKnockedDownEveryTime() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMany(20, 1);
         assertEquals(20, game.score());
     }
 
     @Test
-    public void scoreShouldBe13WhenSpareOnFirstFrame() {
-        game.roll(7);
+    public void scoreShouldBe16WhenSpareOnFirstFrame() {
+        rollSpare(7, 3);
         game.roll(3);
-        game.roll(3);
-        for (int i = 3; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(17, 0);
         assertEquals(16, game.score());
+    }
+
+    @Test
+    public void scoreShouldBe20WhenSpareOnFirstFrameAnd5OnnextThrow() {
+        rollSpare(8, 2);
+        game.roll(5);
+        rollMany(17, 0);
+        assertEquals(20, game.score());
+    }
+
+    @Test // just for understanding
+    public void scoreShouldBe20WhenSpareOnSecondFrameAnd5OnnextThrow() {
+        rollMany(2,3);
+        rollSpare(8, 2);
+        game.roll(5);
+        rollMany(15, 0);
+        assertEquals(26, game.score());
+    }
+
+    @Test
+    public void scoreShouldBeValidWhenStrikeIsThrown() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(3);
+        rollMany(16,0);
+        assertEquals(22, game.score());
+    }
+
+    @Test // just for understanding
+    public void scoreShouldBe300ForAPerfectGame() {
+        rollMany(12,10);
+        assertEquals(300, game.score());
+    }
+
+    private void rollSpare(int throw1, int throw2) {
+        game.roll(throw1);
+        game.roll(throw2);
+    }
+
+    private void rollMany(int noOfRolls, int pinsDown) {
+        for (int i = 0; i < noOfRolls; i++) {
+            game.roll(pinsDown);
+        }
     }
 }
